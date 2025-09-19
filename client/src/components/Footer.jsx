d4ec86248e846d2ca61592228ad83fd706f1454d
DevSushant03 <mrsushant2005@gmail.com>
2025-09-19 15:38:45 +0530

    inor change in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
   return (
     <footer className="relative z-10 bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-6 py-12">
         <div className="border-t border-gray-700 pt-8 flex flex-col md:flex-row justify-between items-center">
-          
           {/* Logo / Name */}
           <div className="flex items-center space-x-3 mb-6 md:mb-0">
-            <span className="text-2xl font-extrabold text-purple-400">ViseVerse</span>
+            <span className="text-2xl font-extrabold text-purple-400">
+              ViseVerse
+            </span>
           </div>
 
           {/* Copyright & Info */}
@@ -18,11 +19,11 @@ export default function Footer() {
             </p>
             <p className="mt-1">Transforming text with AI.</p>
             <p className="mt-1">
-              Developed by -{' '}
+              Developer Portfolio -{" "}
               <a
                 href="https://devsushant03.netlify.app"
                 title="View Developer Portfolio"
-                className="font-semibold text-purple-400 hover:text-purple-300 hover:underline"
+                className="shining-text hover:underline"
               >
                 Sushant Nachanekar
               </a>
