ac99c04c380721757b0d9175af7c5047d589f0a6
DevSushant03 <mrsushant2005@gmail.com>
2025-11-19 15:04:56 +0530

    add pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+const manifestForPlugin = {
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+  manifest: {
+    name: 'ViseVerse',
+    short_name: 'ViseVerse',
+    description: 'ViseVerse is your AI companion for smarter work, productivity, and creativity. Explore AI tools now!',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  },
+};
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),VitePWA(manifestForPlugin)],
 });
