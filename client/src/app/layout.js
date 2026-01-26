d316852838571f1a23801ce7d06f86031eeb3952
DevSushant03 <mrsushant2005@gmail.com>
2026-01-26 15:14:31 +0530

    setup pwa

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "ViseVerse – All-in-one AI Writing & Document Tool",
+  title: "ViseVerse – AI Tools for Productivity",
   description:
     "ViseVerse is a free AI-powered text summarizer and explanation tool. Summarize long articles, notes, PDFs, and understand text instantly.",
   keywords: [
@@ -21,6 +21,17 @@ export const metadata = {
     "ai summary tool",
     "explain text ai",
   ],
+
+  manifest: "/manifest.json",
+
+  openGraph: {
+    title: "ViseVerse – AI Tools for Productivity",
+    description:
+      "ViseVerse is your AI companion for smarter work, productivity, and creativity.",
+    siteName: "ViseVerse",
+    url: "https://viseverse.vercel.app",
+    type: "website",
+  },
   authors: [{ name: "ViseVerse" }],
   verification: {
     google: "hLLV_ZPmUxhmqmfsGWjmuFq52v3N-q4EV51RRjUXgXM",
